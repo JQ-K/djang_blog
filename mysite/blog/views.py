c97f4e64b036808e71662c1a6e591148bb36fb8c
JQ-K <kangqingjie01@gmail.com>
2018-03-31 11:37:49 +0800

    添加博客列表

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,18 @@
+#coding:utf-8
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import F
+from django.views.generic.list import ListView
+
+
+from django.views.generic.edit import FormView
+
+from forms import ArticlePublishForm
+
+from models import Article
 # Create your views here
 
 def blog_index(request):
@@ -10,3 +22,31 @@ def blog_index(request):
         'welcome': 'hello world!',
     }
     return render(request, 'blog_index.html',context)
+
+class ArticleListView(ListView):
+    template_name = 'blog_index.html'
+
+    def get_queryset(self, **kwargs):
+        object_list = Article.objects.all().order_by(F('created').desc())[:100]
+        paginator = Paginator(object_list, 10)
+        page = self.request.GET.get('page')
+        try:
+            object_list = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            object_list = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            object_list = paginator.page(paginator.num_pages)
+        return object_list
+
+
+#publish  class
+class ArticlePublishView(FormView):
+    template_name = 'article_publish.html'
+    form_class = ArticlePublishForm
+    success_url = '/blog/'
+
+    def form_valid(self, form):
+        form.save(self.request.user.username)
+        return super(ArticlePublishView, self).form_valid(form)
